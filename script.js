d0dbed5e6eb8b7c559264b2d877aa2c4768c14dc
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2023-12-15 19:31:00 -0600

    Creacion del elemento popup

---
+++ script.js
@@ -0,0 +1,31 @@
+const profileForm = document.querySelector(".popup__form");
+const nombre = document.querySelector('.input__name');
+const profesion = document.querySelector('.input__profesion');
+const profile__name = document.querySelector('.profile__name');
+const profile__profession = document.querySelector('.profile__profession');
+const popup = document.querySelector(".popup");;
+const buttonEdit = document.querySelector(".profile__button-edit");
+const fix = document.querySelector(".body"); //quitar scroll
+const buttoncerrar = document.querySelector(".button__type-close");
+
+
+
+
+profileForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    profile__name.textContent = nombre.value;
+    profile__profession.textContent = profesion.value;  
+     //reset
+    profileForm.reset();
+  })
+
+  buttonEdit.addEventListener('click', function(){
+    popup.classList.toggle('popup_open');
+    fix.classList.toggle('fix');    
+  });
+
+  buttoncerrar.addEventListener('click', function(){
+    popup.classList.toggle('popup_open');
+    fix.classList.toggle('fix');
+  }
+  )
