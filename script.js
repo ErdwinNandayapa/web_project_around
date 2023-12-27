d001be81fe3cc0812b095234a21e3034249e31c0
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2023-12-26 19:11:34 -0600

    conclusion del sprint 6

--- script.js
+++ script.js
@@ -1,41 +1,34 @@
 const profileForm = document.querySelector(".popup__form");
-const nombre = document.querySelector('.input__name');
-const profesion = document.querySelector('.input__profesion');
-const profile__name = document.querySelector('.profile__name');
-const profile__profession = document.querySelector('.profile__profession');
-const popup = document.querySelector(".popup");;
+const nombre = document.querySelector(".input__name");
+const profesion = document.querySelector(".input__profesion");
+const profile__name = document.querySelector(".profile__name");
+const profile__profession = document.querySelector(".profile__profession");
+const popup = document.querySelector(".popup");
 const buttonEdit = document.querySelector(".profile__button-edit");
 const fix = document.querySelector(".body"); //quitar scroll
 const buttoncerrar = document.querySelector(".button__type-close");
-// const buttonlike = document.querySelector(".button__type-like")
+const buttonimage = document.querySelector(".images__card");
 
+//
+profileForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profile__name.textContent = nombre.value;
+  profile__profession.textContent = profesion.value;
+  //reset
+  profileForm.reset();
 
+  popup.classList.toggle("popup_open");
+  fix.classList.toggle("fix");
+});
 
-//  buttonlike.addEventListener('click', function(){
-//    buttonlike.classList.toggle('button__like');
-//  });
+buttonEdit.addEventListener("click", function () {
+  //abrir popup
+  popup.classList.toggle("popup_open");
+  //quita el scroll
+  fix.classList.toggle("fix");
+});
 
-
-profileForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    profile__name.textContent = nombre.value;
-    profile__profession.textContent = profesion.value;  
-     //reset
-    profileForm.reset();
-    
-    popup.classList.toggle('popup_open');
-    fix.classList.toggle('fix');
-  })
-
-  buttonEdit.addEventListener('click', function(){
-    //abrir popup
-    popup.classList.toggle('popup_open');
-    //quita el scroll
-    fix.classList.toggle('fix');    
-  });
-
-  buttoncerrar.addEventListener('click', function(){
-    popup.classList.toggle('popup_open');
-    fix.classList.toggle('fix');
-  }
-  )
+buttoncerrar.addEventListener("click", function () {
+  popup.classList.toggle("popup_open");
+  fix.classList.toggle("fix");
+});
