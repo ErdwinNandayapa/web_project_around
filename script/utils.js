91a7c073db9ecd29b7d3227b7fd89b319de3d748
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2024-01-20 00:53:16 -0600

    animation card

--- script/utils.js
+++ script/utils.js
@@ -17,6 +17,21 @@ export function createPopupImage(evet) {
   document.addEventListener("keydown", closeAnimationendPopuOpen);
 }
 
+export function closeButtonCards(event) {
+  const card = event.target.parentElement.parentElement;
+  if (card) {
+    card.classList.add("popup-closeTransition");
+    card.addEventListener("animationend", () => {
+      card.classList.remove("popup-closeTransition");
+      card.remove();
+    });
+  }
+}
+
+export function buttonLike(evet) {
+  evet.target.classList.toggle("button__like");
+}
+
 export const validationConfig = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
