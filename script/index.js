b2d35b794861f3f045f3720f7c1864e637c43edf
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2024-02-02 09:31:22 -0600

    Update pop-up elements in index.html and index.js

--- script/index.js
+++ script/index.js
@@ -11,13 +11,13 @@ export const buttonAdd = document.querySelector(".profile__button-add");
 export const nameProfession = document.querySelector(".popup__input-name");
 export const profesion = document.querySelector(".popup__input-profesion");
 export const buttonEdit = document.querySelector(".profile__button-edit");
-const popupFormAdd = new Popup("#popup__add");
-const popup = new Popup(".popup");
+const imagePopup = new PopupWithImage("#popup_image");
+const popupWithFormAdd = new PopupWithForm("#popup__add", formSubmitHandlerAdd);
+const popupWithFormEdit = new PopupWithForm("#popup__edit", formSubmitHandler);
 const userInfo = new UserInfo({
   nameSelector: ".profile__name",
   jobSelector: ".profile__profession",
 });
-
 const cardsContent = [
   {
     name: "Valle de Yosemite",
@@ -44,25 +44,6 @@ const cardsContent = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
   },
 ];
-buttonAdd.addEventListener("click", popupButtonAdd);
-
-function popupButtonAdd(event) {
-  event.preventDefault();
-  popupFormAdd.open(); //add
-  const formElement2 = document.querySelector(".popup__form-add");
-  new FormValidator(validationConfig, formElement2);
-}
-// formulario profile
-buttonEdit.addEventListener("click", openProfile);
-function openProfile() {
-  const userData = userInfo.getUserInfo();
-  nameProfession.value = userData.name;
-  profesion.value = userData.job;
-  const formElement = document.querySelector(".popup__form");
-  new FormValidator(validationConfig, formElement);
-  popup.open();
-}
-const imagePopup = new PopupWithImage(".popup_image");
 
 const defaultCardList = new Section(
   {
@@ -80,8 +61,24 @@ const defaultCardList = new Section(
   },
   ".cards"
 );
+buttonAdd.addEventListener("click", popupButtonAdd);
+buttonEdit.addEventListener("click", openProfile);
 
-defaultCardList.renderItems();
+function popupButtonAdd(event) {
+  event.preventDefault();
+  popupWithFormAdd.open(); //add
+  const formElement2 = document.querySelector(".popup__form-add");
+  new FormValidator(validationConfig, formElement2);
+}
+
+function openProfile() {
+  const userData = userInfo.getUserInfo();
+  nameProfession.value = userData.name;
+  profesion.value = userData.job;
+  const formElement = document.querySelector(".popup__form");
+  new FormValidator(validationConfig, formElement);
+  popupWithFormEdit.open();
+}
 
 function formSubmitHandler(formValues) {
   userInfo.setUserInfo({
@@ -89,10 +86,8 @@ function formSubmitHandler(formValues) {
     job: formValues["input-job"],
   });
   console.log(formValues);
-  popup.close();
+  popupWithFormEdit.close();
 }
-const popupWithFormEdit = new PopupWithForm(".popup__edit", formSubmitHandler);
-popupWithFormEdit.setEventListeners();
 
 function formSubmitHandlerAdd(formValues) {
   const newCard = new Card(
@@ -102,8 +97,9 @@ function formSubmitHandlerAdd(formValues) {
     imagePopup.open
   ).createCardElement();
   defaultCardList.setItem(newCard);
-  popup.close();
+  popupWithFormEdit.close();
 }
 
-const popupWithFormAdd = new PopupWithForm("#popup__add", formSubmitHandlerAdd);
+defaultCardList.renderItems();
+popupWithFormEdit.setEventListeners();
 popupWithFormAdd.setEventListeners();
