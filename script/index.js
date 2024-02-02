d64d02008a3f5238a7954c61e7dc6f038126bfde
Erdwin Nandayapa Cordero <nandayapaerdwin956@gmail.com>
2024-02-02 17:41:09 -0600

    Add imports and variables for form validation and card content

--- script/index.js
+++ script/index.js
@@ -5,14 +5,14 @@ import PopupWithImage from "./PopupWithImage.js";
 import Section from "./Section.js";
 import UserInfo from "./UserInfo.js";
 import PopupWithForm from "./PopupWithForm.js";
+import { cardsContent, formValidaProfile, formValidaPlace } from "./const.js";
 
 export const buttonAdd = document.querySelector(".profile__button-add");
 export const nameProfession = document.querySelector(".popup__input-name");
 export const profesion = document.querySelector(".popup__input-profesion");
 export const buttonEdit = document.querySelector(".profile__button-edit");
 const imagePopup = new PopupWithImage(".popup_image");
-const formValidaProfile = document.querySelector(".popup__form");
-const formValidaPlace = document.querySelector(".popup__form-add");
+
 const popupWithFormAdd = new PopupWithForm(".popup__add", formSubmitHandlerAdd);
 const popupWithFormEdit = new PopupWithForm(".popup__edit", formSubmitHandler);
 const userInfo = new UserInfo({
@@ -20,32 +20,6 @@ const userInfo = new UserInfo({
   jobSelector: ".profile__profession",
 });
 new FormValidator(validationConfig, formValidaPlace);
-const cardsContent = [
-  {
-    name: "Valle de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-  },
-  {
-    name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-  },
-  {
-    name: "Montañas Calvas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-  },
-  {
-    name: "Parque Nacional de la Vanoise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-  },
-];
 
 const defaultCardList = new Section(
   {
